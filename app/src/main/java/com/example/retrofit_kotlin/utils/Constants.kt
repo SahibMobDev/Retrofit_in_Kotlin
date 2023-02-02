b6e672f75e6f4da2462c7ac203f5d705788479ca
Sahib <sahibmobdev@gmail.com>
2023-02-02 17:22:36 +0400

    update

--- app/src/main/java/com/example/retrofit_kotlin/utils/Constants.kt
+++ app/src/main/java/com/example/retrofit_kotlin/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.retrofit_kotlin.utils
 
 object Constants {
-
+    //ApiKey
     const val API_KEY = "b90c4dfa229a06303d6390561c4f39cb"
     const val BASEURL = "https://api.themoviedb.org/3/"
     const val POSTER_BASEURL = "https://image.tmdb.org/t/p/w342"
